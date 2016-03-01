dcd192d2d787643f5d75f90a60239f54a2587e72
Chima Akparanta <chima@udel.edu>
2016-03-01 03:12:22 -0500

    finished

---
+++ Q2/src/TuitionRepay.java
@@ -0,0 +1,24 @@
+import.java.util.Scanner;
+
+public class TuitionRepay {
+	Scanner input = new Scanner(System.in);
+	
+	
+	static double fv( double r,double n,double y,double p,boolean t){
+		boolean t == true
+				double p = 0;
+		double PMT = 30130;
+		double n = 4;
+		//n is the term
+	
+		System.out.print("Enter the rate of tuition inflation: ");
+		double r = input.newDouble();
+	
+		System.out.print("Enter the payment in each period: ");
+		double y = input.newDouble();
+		}
+	 
+	 System.out.printIn("The total cost of tuition is :" + fv);
+	
+
+}
